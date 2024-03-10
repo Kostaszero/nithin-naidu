f43ce8e3e3543e26675d95da944aca53048b15fe
Kostaszero <nitinnaidu0602@gmail.com>
2024-03-10 23:20:00 +0530

    updated live

--- src/Views/ViewPage.tsx
+++
@@ -1,9 +0,0 @@
-import Info from "./Info/Info"
-import Friends from "./Friends/Friends"
-import Projects from "./Projects/Projects"
-import Timeline from "./Timeline/Timeline"
-
-export default function ViewPage({idx}: any){
-    const views = [<Info/>, <Timeline/> , <Friends/>, <Projects/>]
-    return views[idx]
-}
